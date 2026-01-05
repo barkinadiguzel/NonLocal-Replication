da6e40dbc7bccd288811def4160d29dc328dc622
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-05 11:16:08 +0300

    Update nonlocal_cnn.py

--- src/model/nonlocal_cnn.py
+++ src/model/nonlocal_cnn.py
@@ -1,26 +1,30 @@
 import torch.nn as nn
-from ..backbone.resnet_blocks import BasicResBlock
+from ..backbone.resnet_blocks import BasicResBlock, ResNetStage
 from ..nonlocal.nonlocal_block import NonLocalBlock
 
 class NonLocalCNN(nn.Module):
-    def __init__(self, in_ch=3, base_ch=64, num_blocks=5, f_type="embedded", dim=3):
+    def __init__(self, in_ch=3, base_ch=64, nonlocal_positions=None, f_type="embedded", dim=3):
         super().__init__()
-        self.dim = dim
+        nonlocal_positions = nonlocal_positions or ["res2", "res3", "res4"]
 
         self.stem = BasicResBlock(in_ch, base_ch, dim=dim)
+        self.res2 = ResNetStage(base_ch, base_ch, num_blocks=2, dim=dim)
+        self.res3 = ResNetStage(base_ch, base_ch*2, num_blocks=2, dim=dim)
+        self.res4 = ResNetStage(base_ch*2, base_ch*4, num_blocks=2, dim=dim)
+        
+        self.nl_res2 = NonLocalBlock(base_ch, f_type=f_type, dim=dim) if "res2" in nonlocal_positions else None
+        self.nl_res3 = NonLocalBlock(base_ch*2, f_type=f_type, dim=dim) if "res3" in nonlocal_positions else None
+        self.nl_res4 = NonLocalBlock(base_ch*4, f_type=f_type, dim=dim) if "res4" in nonlocal_positions else None
 
-        self.nonlocals = nn.ModuleList([
-            NonLocalBlock(base_ch, f_type=f_type, dim=dim) for _ in range(num_blocks)
-        ])
-
-        if dim == 3:
-            self.head = nn.AdaptiveAvgPool3d(1)
-        else:
-            self.head = nn.AdaptiveAvgPool2d(1)
+        self.head = nn.AdaptiveAvgPool3d(1)
 
     def forward(self, x):
         x = self.stem(x)
-        for block in self.nonlocals:
-            x = block(x)
+        x = self.res2(x)
+        if self.nl_res2: x = self.nl_res2(x)
+        x = self.res3(x)
+        if self.nl_res3: x = self.nl_res3(x)
+        x = self.res4(x)
+        if self.nl_res4: x = self.nl_res4(x)
         x = self.head(x)
         return x.view(x.size(0), -1)
